233f184cce0dfb3e0d4655898dc5494a76daaf44
RE045908 <re045908@qintess.com>
2025-09-29 21:26:16 -0300

    SMS level 3 - 4.9 - Dynamic and Validated Contracts and Stubs

--- src/main/java/com/algaworks/algashop/product/catalog/presentation/ProductController.java
+++ src/main/java/com/algaworks/algashop/product/catalog/presentation/ProductController.java
@@ -1,5 +1,6 @@
 package com.algaworks.algashop.product.catalog.presentation;
 
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,20 +14,25 @@ public class ProductController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public ProductDetailOutput create(@RequestBody ProductInput input){
+    public ProductDetailOutput create(@RequestBody @Valid ProductInput input){
         return ProductDetailOutput.builder()
                 .id(UUID.randomUUID())
                 .addedAt(OffsetDateTime.now())
                 .inStock(false)
                 .name(input.getName())
                 .brand(input.getBrand())
+                .name(input.getName())
+                .brand(input.getBrand())
                 .description(input.getDescription())
                 .regularPrice(input.getRegularPrice())
                 .salePrice(input.getSalePrice())
                 .enabled(input.getEnabled())
-                .category(CategoryMinimalOutput.builder()
-                        .name("Notebook")
-                        .build())
+                .category(
+                        CategoryMinimalOutput.builder()
+                                .id(input.getCategoryId())
+                                .name("Notebook")
+                                .build()
+                )
                 .build();
 
     }
