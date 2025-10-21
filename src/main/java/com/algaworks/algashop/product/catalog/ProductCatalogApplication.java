a42c6f53055f804287df8138a8ea9a8ea1c02646
RE045908 <re045908@qintess.com>
2025-10-21 16:41:15 -0300

    SMS level 3 - 5.12 - Improving Client API Responses

--- src/main/java/com/algaworks/algashop/product/catalog/ProductCatalogApplication.java
+++ src/main/java/com/algaworks/algashop/product/catalog/ProductCatalogApplication.java
@@ -3,11 +3,14 @@ package com.algaworks.algashop.product.catalog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class ProductCatalogApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProductCatalogApplication.class, args);
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+        SpringApplication.run(ProductCatalogApplication.class, args);
 	}
 
 }
