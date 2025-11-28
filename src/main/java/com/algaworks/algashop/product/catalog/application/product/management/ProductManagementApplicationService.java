1bee5ba77b3d9eaf7f4cb0b4e61da50171a1e305
RE045908 <re045908@qintess.com>
2025-11-28 08:55:55 -0300

    SMS level 4 - 2.14 - Implementing Product Update

--- src/main/java/com/algaworks/algashop/product/catalog/application/product/management/ProductManagementApplicationService.java
+++ src/main/java/com/algaworks/algashop/product/catalog/application/product/management/ProductManagementApplicationService.java
@@ -4,6 +4,7 @@ import com.algaworks.algashop.product.catalog.domain.model.category.Category;
 import com.algaworks.algashop.product.catalog.domain.model.category.CategoryNotFoundException;
 import com.algaworks.algashop.product.catalog.domain.model.category.CategoryRepository;
 import com.algaworks.algashop.product.catalog.domain.model.product.Product;
+import com.algaworks.algashop.product.catalog.domain.model.product.ProductNotFoundException;
 import com.algaworks.algashop.product.catalog.domain.model.product.ProductRepository;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
@@ -26,15 +27,40 @@ public class ProductManagementApplicationService {
         return product.getId();
     }
 
-
-
     public void update(UUID productId, ProductInput input){
-
+        Product product = findProduct(productId);
+        Category category = findCategory(input.getCategoryId());
+        updateProduct(product, input);
+        productRepository.save(product);
     }
 
     public void disable(UUID productId){
-
+        Product product = findProduct(productId);
+        product.disable();
+        productRepository.save(product);
     }
+
+    public void enable(UUID productId){
+        Product product = findProduct(productId);
+        product.enable();
+        productRepository.save(product);
+    }
+
+    private void updateProduct(Product product, ProductInput input) {
+        product.setName(input.getName());
+        product.setBrand(input.getBrand());
+        product.setDescription(input.getDescription());
+        product.setRegularPrice(input.getRegularPrice());
+        product.setSalePrice(input.getSalePrice());
+        product.setEnabled(input.getEnabled());
+    }
+
+    private Product findProduct(UUID productId) {
+        return productRepository.findById(productId)
+                .orElseThrow(() -> new ProductNotFoundException(productId));
+    }
+
+
 
     private Product mapToProduct(ProductInput input) {
         Category category = findCategory(input.getCategoryId());
