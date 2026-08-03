d08011a0c7291ccb538fafbcd49fb32046bae407
RE045908 <re045908@qintess.com>
2026-08-03 17:45:08 -0300

    SMS level 4 - 4.7 - Index for text-based search via Spring

--- src/main/java/com/algaworks/algashop/product/catalog/application/product/query/ProductSummaryOutput.java
+++ src/main/java/com/algaworks/algashop/product/catalog/application/product/query/ProductSummaryOutput.java
@@ -44,6 +44,6 @@ public class ProductSummaryOutput {
 
     private Integer discountPercentageRounded;
 
-    
+    private Float score;
 
 }
--- src/main/java/com/algaworks/algashop/product/catalog/infrastructure/persistence/product/ProductQueryServiceImpl.java
+++ src/main/java/com/algaworks/algashop/product/catalog/infrastructure/persistence/product/ProductQueryServiceImpl.java
@@ -18,6 +18,7 @@ import org.springframework.data.mongodb.core.aggregation.AggregationExpressionCr
 import org.springframework.data.mongodb.core.aggregation.ComparisonOperators;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.TextCriteria;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -74,6 +75,9 @@ public class ProductQueryServiceImpl implements ProductQueryService {
     }
 
     private Sort sortWith(ProductFilter filter) {
+        if (StringUtils.isNotBlank(filter.getTerm())){
+            return Sort.by( "score");
+        }
         return Sort.by(filter.getSortDirectionOrDefault(), filter.getSortByPropertyOrDefault().getPropertyName());
     }
 
@@ -135,14 +139,7 @@ public class ProductQueryServiceImpl implements ProductQueryService {
         }
 
         if (StringUtils.isNotBlank(filter.getTerm())){
-            String regexExpression = String.format(findWordRegex, filter.getTerm());
-            query.addCriteria(
-                    new Criteria().orOperator(
-                            Criteria.where("name").regex(regexExpression),
-                            Criteria.where("brand").regex(regexExpression),
-                            Criteria.where("description").regex(regexExpression)
-                    )
-            );
+            query.addCriteria(TextCriteria.forDefaultLanguage().matching(filter.getTerm()));
         }
         return query;
     }
