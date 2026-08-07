9c1454e9990e004ebc8f54738edca8efa40041e7
RE045908 <re045908@qintess.com>
2026-08-07 09:56:26 -0300

    SMS level 4 - 6.8 - Eventual consistency via events

--- src/main/java/com/algaworks/algashop/product/catalog/infrastructure/listener/category/CategoryEventListener.java
+++ src/main/java/com/algaworks/algashop/product/catalog/infrastructure/listener/category/CategoryEventListener.java
@@ -1,16 +1,23 @@
 package com.algaworks.algashop.product.catalog.infrastructure.listener.category;
 
 import com.algaworks.algashop.product.catalog.application.category.event.CategoryUpdatedEvent;
+import com.algaworks.algashop.product.catalog.infrastructure.persistence.category.ProductCategoryUpdater;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
+@AllArgsConstructor
 public class CategoryEventListener {
 
+    private ProductCategoryUpdater productCategoryUpdater;
+
     @EventListener
+    @Async
     public void handle(CategoryUpdatedEvent categoryUpdatedEvent) {
-        log.info("Category Updated Received: {}", categoryUpdatedEvent.getCategoryId());
+        productCategoryUpdater.copyCategoryDataToProducts(categoryUpdatedEvent);
     }
 }
