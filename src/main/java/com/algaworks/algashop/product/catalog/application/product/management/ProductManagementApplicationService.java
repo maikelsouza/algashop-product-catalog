19811a2d919932a8d3ea4450ac59bd0da54c23b5
RE045908 <re045908@qintess.com>
2026-08-21 09:33:33 -0300

    SMS level 4 - 9.17 - Cache invalidation via trigger with CacheEvict

--- src/main/java/com/algaworks/algashop/product/catalog/application/product/management/ProductManagementApplicationService.java
+++ src/main/java/com/algaworks/algashop/product/catalog/application/product/management/ProductManagementApplicationService.java
@@ -8,6 +8,7 @@ import com.algaworks.algashop.product.catalog.domain.model.category.CategoryRepo
 import com.algaworks.algashop.product.catalog.domain.model.product.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -39,6 +40,8 @@ public class ProductManagementApplicationService {
 
     @CachePut(cacheNames = "algashop:products:v1", key = "#result.id",
             condition = "#input.enabled == true")
+    @CacheEvict(cacheNames = "algashop:products:v1", key = "#productId",
+            condition = "#input.enabled == false")
     public ProductDetailOutput update(UUID productId, ProductInput input){
         Product product = findProduct(productId);
         Category category = findCategory(input.getCategoryId());
@@ -48,6 +51,7 @@ public class ProductManagementApplicationService {
         return mapper.convert(product, ProductDetailOutput.class);
     }
 
+    @CacheEvict(cacheNames = "algashop:products:v1", key = "#productId")
     public void disable(UUID productId){
         Product product = findProduct(productId);
         product.disable();
