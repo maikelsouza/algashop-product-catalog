bb9a3062a5dea19ea2870e03d206ae57467ac74d
RE045908 <re045908@qintess.com>
2026-08-25 16:57:38 -0300

    SMS level 4 - 9.21 - Cache: Eventual consistency or programmatic clearing

--- src/main/java/com/algaworks/algashop/product/catalog/application/category/management/CategoryManagementApplicationService.java
+++ src/main/java/com/algaworks/algashop/product/catalog/application/category/management/CategoryManagementApplicationService.java
@@ -6,6 +6,8 @@ import com.algaworks.algashop.product.catalog.domain.model.category.Category;
 import com.algaworks.algashop.product.catalog.domain.model.category.CategoryNotFoundException;
 import com.algaworks.algashop.product.catalog.domain.model.category.CategoryRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -18,12 +20,17 @@ public class CategoryManagementApplicationService {
 
     private final ApplicationMessagePublisher applicationMessagePublisher;
 
+    @CacheEvict(value = "algashop:categories-filter:v1", key = "'default'")
     public UUID create(CategoryInput input){
         Category category = new Category(input.getName(), input.getEnabled());
         categoryRepository.save(category);
         return category.getId();
     }
 
+    @Caching(evict = {
+            @CacheEvict(value = "algashop:categories-filter:v1", key = "'default'"),
+            @CacheEvict(value = "algashop:categories:v1", key = "#categoryId")
+    })
     public void update(UUID categoryId, CategoryInput input){
         Category category = categoryRepository.findById(categoryId)
                 .orElseThrow(() -> new CategoryNotFoundException(categoryId));
